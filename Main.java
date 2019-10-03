90cc7d65acfc45b29a29deb0145d4bf2422fed21
maximhesed <maximtemny@mail.ru>
2019-10-03 21:05:07 +0300

    Raids support
    Bot's support moved to cinematic mode.
    This allows don't care about browser scrolling.
    
    Fishing is separate function.
    Call mode through menu.
    
    Added colors compare method, wherein check is done immediately.
    
    Robot definition inside the Main class.
    Now methods don't need Robot argument.
    
    Delay before and after click together in click().
    
    Added check method, which automatically:
    1. Watch the ads.
    2. Exit from completed dungeons.
    3. Capture the familiars.
    It can be used regardless of the start-methods.

--- Main.java
+++ Main.java
@@ -1,166 +1,158 @@
-import java.awt.AWTException;
 import java.awt.Robot;
-import java.awt.PointerInfo;
-import java.awt.MouseInfo;
 import java.awt.Point;
-import java.awt.event.InputEvent;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.Color;
-import javax.swing.Timer;
+import java.awt.event.InputEvent;
 
 final class Main
 {
-    static int successfulWaited_tranzistor = 0;
-    static Color color = new Color(0, 0, 0); // it's more comfortable for me
-    static int ms = 0;
+    private static final Point button_start = new Point(887, 727);
+    private static final Point button_trade = new Point(895, 685);
+    private static final Point button_close = new Point(1141, 391);
+    private static final Point button_got_away = new Point(902, 596);
+    private static final Point bar_distance = new Point(770, 680);
+    private static final Point best_percentage = new Point(1258, 634);
+    private static final Point button_raid = new Point(590, 567);
+    private static final Point button_summon = new Point(1094, 620);
+    private static final Point button_heroic = new Point(1142, 481);
+    private static final Point button_accept = new Point(1067, 706);
+    private static final Point button_yes = new Point(836, 596);
+    private static final Point button_watch = new Point(678, 676);
+    private static final Point button_ad_close = new Point(1284, 158);
+    private static final Point button_persuade = new Point(731, 565);
+    private static final Point button_cinematic_open = new Point(448, 442);
+    //private static final Point button_cinematic_close = new Point(1405, 219);
+    private static final Point button_fishing = new Point(1316, 649);
+    private static final Point button_play = new Point(874, 556);
 
-    private static void Click(Robot r, Point p, int time)
+    private static final Color best_percentage_color = new Color(77, 254, 0);
+    private static final Color button_trade_color = new Color(163, 209, 47);
+    private static final Color button_yes_color = new Color(165, 211, 51);
+    private static final Color button_watch_color = new Color(51, 179, 211);
+    private static final Color button_persuade_color = new Color(46, 174, 209);
+    private static final Color button_got_away_color = new Color(51, 179, 211);
+
+    private static Robot bot;
+
+    private static void click(int btime, Point p, int atime) throws Exception
     {
-        try {
-            r.mouseMove(p.x, p.y);
-            r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(btime);
 
-            Thread.sleep(time);
-        } catch (InterruptedException exception) {
-            System.err.println(exception.getMessage());
+        bot.mouseMove(p.x, p.y);
+        bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        Thread.sleep(atime);
+    }
+
+    private static boolean compareColors(Point p, Color c)
+    {
+        Color color = bot.getPixelColor(p.x, p.y);
+        if (color.getRed() == c.getRed() &&
+            color.getGreen() == c.getGreen() &&
+            color.getBlue() == c.getBlue())
+            return true;
+
+        return false;
+    }
+
+    private static void check() throws Exception
+    {
+        while (true) {
+            Thread.sleep(500);
+
+            // check if dungeon was finished
+            if (compareColors(button_yes, button_yes_color)) {
+                click(0, button_yes, 2500);
+
+                break;
+            }
+
+            // check for ad
+            if (compareColors(button_watch, button_watch_color)) {
+                click(0, button_watch, 32500);
+                click(0, button_ad_close, 500);
+                click(0, button_ad_close, 750);
+                click(0, button_cinematic_open, 0);
+            }
+
+            // check for treasure
+
+            // check for capture
+            if (compareColors(button_persuade, button_persuade_color)) {
+                click(250, button_persuade, 750);
+                click(0, button_yes, 0);
+            }
+        }
+    }
+
+    private static void startFishing(int baits) throws Exception
+    {
+        int i;
+
+        click(0, button_fishing, 500);
+        click(0, button_play, 6000);
+
+        for (i = 0; i < baits; i++) {
+            // TODO: check user for baits availability
+
+            // start fishing
+            click(0, button_start, 692);
+            click(0, button_start, 7500);
+
+            // if caught garbage
+            if (compareColors(button_trade, button_trade_color)) {
+                click(0, button_trade, 500);
+                click(0, button_close, 500);
+
+                continue;
+            }
+
+            while (true) {
+                if (compareColors(best_percentage, best_percentage_color)) {
+                    click(0, button_start, 0);
+
+                    break;
+                }
+            }
+
+            Thread.sleep(5000);
+
+            // or fish got away
+            if (compareColors(button_got_away, button_got_away_color))
+                click(0, button_got_away, 500);
+            else {
+                // either trade it
+                click(0, button_trade, 500);
+                click(0, button_close, 500);
+            }
+        }
+    }
+
+    private static void startRaid(int shards) throws Exception
+    {
+        int i;
+
+        for (i = 0; i < shards; i++) {
+            click(0, button_raid, 750);
+            click(0, button_summon, 750);
+            click(0, button_heroic, 750);
+            click(0, button_accept, 750);
+
+            check();
         }
     }
 
     public static void main(String[] args) throws Exception
     {
-        final Point button_start = new Point(770, 770);
-        final Point button_trade = new Point(745, 734);
-        final Point button_close = new Point(990, 438);
-        final Point button_got_away = new Point(770, 654);
-        final Point bar_distance = new Point(770, 680);
-        final Point best_percentage = new Point(1108, 681);
-        final Point frame_garbage = new Point(745, 535);
-        final Color best_percentage_color = new Color(77, 254, 0);
-        final Color button_trade_color = new Color(165, 211, 51);
+        bot = new Robot();
 
-        Robot bot = new Robot();
-        int baits;
-        int i;
-
-        if (args.length == 1)
-            baits = Integer.parseInt(args[0]);
-        else
-            return;
+        // TODO: parse options
 
         // wait a bit, while user open game window
         Thread.sleep(1500);
 
-        Timer timer = new Timer(1, new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                color = bot.getPixelColor(best_percentage.x, best_percentage.y);
-                if (color.getGreen() == best_percentage_color.getGreen()) {
-                    ms += 1;
-
-                    //System.out.printf("TIMER: ms = %d\n", ms);
-
-                    if (ms > 0) {
-                        Click(bot, button_start, 0);
-
-                        successfulWaited_tranzistor = 1;
-                    }
-                } else {
-                    //System.out.printf("TIMER: Colors changed!\n", ms);
-
-                    successfulWaited_tranzistor = 2;
-                }
-
-                //System.out.printf("TIMER: I'm working...\n", ms);
-            }
-        });
-
-        for (i = 0; i < baits; i++) {
-            boolean isGotAway = false;
-
-            successfulWaited_tranzistor = 0;
-            ms = 0;
-
-            // TODO: check user for baits availability
-
-            // start fishing
-            Click(bot, button_start, 0);
-
-            /**
-             * In theory was excepted, that distance always is will be max.
-             * Of course not. But it makes you less perceptible. :)
-             */
-            Thread.sleep(744);
-
-            Click(bot, button_start, 7500);
-
-            // if caught garbage
-            color = bot.getPixelColor(frame_garbage.x, frame_garbage.y);
-
-            //System.out.printf("r: %d; g: %d; b: %d\n",
-            //    color.getRed(), color.getGreen(), color.getBlue());
-
-            if (color.getRed() > 200 && color.getGreen() > 200 && color.getBlue() > 200) {
-                Click(bot, button_trade, 500);
-                Click(bot, button_close, 500);
-
-                continue;
-            }
-
-catching:
-            while (true) {
-                color = bot.getPixelColor(best_percentage.x, best_percentage.y);
-                if (color.getGreen() == best_percentage_color.getGreen()) {
-                    timer.start();
-
-                    // wait end of timer
-                    while (true) {
-                        if (successfulWaited_tranzistor == 1) {
-                            System.out.printf("Fish was catched with best rate. Break.\n");
-
-                            timer.stop();
-
-                            break catching;
-                        } else if (successfulWaited_tranzistor == 2) {
-                            System.out.printf("Rate was slipped. Continue.\n");
-
-                            timer.stop();
-
-                            Thread.sleep(250);
-
-                            // check if fish got away
-                            color = bot.getPixelColor(button_start.x, button_start.y);
-                            if (color.getRed() < 20 &&
-                                color.getGreen() < 20 &&
-                                color.getBlue() < 20) {
-
-                                Click(bot, button_got_away, 500);
-
-                                isGotAway = true;
-
-                                break catching;
-                            }
-
-                            continue catching;
-                        } else
-                            System.out.printf("Timer is working...\n");
-                    }
-                }
-
-                //Thread.sleep(25);
-            }
-
-            if (isGotAway)
-                continue;
-
-            Thread.sleep(5000);
-
-            // or fish got away
-            Click(bot, button_got_away, 500);
-
-            // either trade it
-            Click(bot, button_trade, 500);
-            Click(bot, button_close, 500);
-        }
+        // call anything
+        //startFishing(Integer.parseInt(args[0]));
+        startRaid(Integer.parseInt(args[0]));
     }
 }
